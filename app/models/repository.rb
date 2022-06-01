e9beca504a2c25a8590ccfb9bcd9c07d1820d547
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-06-01 14:29:55 +0300

    Added repository state

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,10 +1,28 @@
 # frozen_string_literal: true
 
 class Repository < ApplicationRecord
+  include AASM
   extend Enumerize
 
   belongs_to :user
   has_many :checks, class_name: 'Repository::Check', dependent: :destroy
 
   enumerize :language, in: %i[javascript ruby]
+
+  aasm do
+    state :created, initial: true
+    state :fetching, :fetched, :failed
+
+    event :fetch do
+      transitions from: %i[created failed fetched], to: :fetching
+    end
+
+    event :complete do
+      transitions from: :fetching, to: :fetched
+    end
+
+    event :fail do
+      transitions from: :fetching, to: :failed
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_29_141550) do
+ActiveRecord::Schema.define(version: 2022_06_01_093548) do
 
   create_table "repositories", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_05_29_141550) do
     t.datetime "repo_updated_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "aasm_state"
     t.index ["user_id"], name: "index_repositories_on_user_id"
   end
 
