5e4f15e50f3d27997429b48b80d15dbd81edea6e
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-06-04 12:30:03 +0300

    Fixed the stub check result

--- app/lib/repository_checker_stub.rb
+++ app/lib/repository_checker_stub.rb
@@ -7,6 +7,6 @@ class RepositoryCheckerStub
 
   def start_checking
     issue_messages = File.read(Rails.root.join('test', 'fixtures', 'files', 'check_results.json'))
-    { issue_count: 1, issue_messages: issue_messages }
+    { issue_count: 0, issue_messages: issue_messages }
   end
 end
