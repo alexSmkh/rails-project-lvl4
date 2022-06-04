b2b574d5b3f4f0f8a0465b66dd24fc4348aba5ec
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-06-04 13:40:46 +0300

    Fixed the repository show view

--- app/controllers/api/checks_controller.rb
+++ app/controllers/api/checks_controller.rb
@@ -6,7 +6,7 @@ class Api::ChecksController < Api::ApplicationController
   def checks
     repository = Repository.find_by(github_id: params[:repository][:id])
 
-    return head :ok if repository.nil?
+    return head :not_found if repository.nil?
 
     check = repository.checks.create
     RepositoryCheckJob.perform_later(check)
