d6c43f10ef9b7faff3d66a7fa1f13f4838ca333b
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-06-05 11:52:39 +0300

    Fixed api/checks controller

--- app/controllers/api/checks_controller.rb
+++ app/controllers/api/checks_controller.rb
@@ -4,9 +4,9 @@ class Api::ChecksController < Api::ApplicationController
   skip_before_action :verify_authenticity_token, only: %i[checks]
 
   def checks
-    repository = Repository.find_by(github_id: params[:repository][:id])
+    repository = Repository.find_by(full_name: params[:repository][:full_name])
 
-    return head :ok if repository.nil?
+    return head :not_found if repository.nil?
 
     check = repository.checks.create
     RepositoryCheckJob.perform_later(check)
