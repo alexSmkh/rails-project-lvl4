6524f388c295fa9cabf130cedce23d6765ecb493
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-06-08 23:27:48 +0300

    Removed user email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,5 @@ class User < ApplicationRecord
   has_many :repositories, dependent: :destroy
 
   validates :email,
-            presence: true,
-            uniqueness: true,
-            length: {
-              maximum: 50
-            },
-            format: {
-              with: URI::MailTo::EMAIL_REGEXP
-            }
+            presence: true
 end
